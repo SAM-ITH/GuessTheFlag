852e47c6e1215a76e97d38bf7f4059f9ec8be363
Samith <samithwijesinha@gmail.com>
2021-07-22 21:43:46 +0530

    first commit

---
+++ GuessTheFlag/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GuessTheFlag
+//
+//  Created by Samith Wijesinghe on 2021-07-22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
